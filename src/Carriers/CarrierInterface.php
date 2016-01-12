91b04b7bd8a69999a93daf74cefed33b0d81fc3b
Tristan Strathearn <tstrathearn@bcm.com.au>
2016-01-13 09:11:37 +1000

    Add has value check method on carrier interface.

--- src/Carriers/CarrierInterface.php
+++ src/Carriers/CarrierInterface.php
@@ -15,4 +15,13 @@ interface CarrierInterface
      * @return mixed
      */
     public function getValueFor($key);
+
+    /**
+     * Check if there is an associated value for the given key.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function hasValueFor($key);
 }
