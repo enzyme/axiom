9d0d47c9703ea7299ad3ee6f2e0a02ffdaa56dca
Tristan Strathearn <tstrathearn@bcm.com.au>
2016-01-13 09:30:00 +1000

    Introduce tests and test framework structure.

---
+++ tests/Atoms/StringAtomTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Enzyme\Axiom\Atoms\StringAtom;
+
+class StringAtomTest extends PHPUnit_Framework_TestCase
+{
+    public function test_atom_stores_correct_base_value()
+    {
+        $expected = 'Foo Bar';
+        $atom = new StringAtom($expected);
+        $this->assertEquals($expected, $atom->getValue());
+
+        $expected = '25';
+        $atom = new StringAtom($expected);
+        $this->assertEquals($expected, $atom->getValue());
+    }
+
+    /**
+     * @expectedException \Enzyme\Axiom\Exceptions\AtomException
+     */
+    public function test_atom_throws_exception_when_initialised_with_invalid_value()
+    {
+        new StringAtom(5);
+    }
+
+    /**
+     * @expectedException \Enzyme\Axiom\Exceptions\AtomException
+     */
+    public function test_atom_throws_exception_when_initialised_with_empty_string()
+    {
+        new StringAtom('');
+    }
+}
