24174a5117b1cb82f2c094f0a0bd1df28f4ff79f
Tristan Strathearn <tstrathearn@bcm.com.au>
2016-01-12 16:28:57 +1000

    Add new boolean atom type.

---
+++ src/Atoms/BooleanAtom.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Enzyme\Axiom\Atoms;
+
+use Enzyme\Axiom\Exceptions\AtomException;
+
+/**
+ * A wrapper around boolean values.
+ */
+class BooleanAtom implements AtomInterface
+{
+    /**
+     * Holds the underlying value.
+     *
+     * @var string
+     */
+    protected $value;
+
+    /**
+     * Create a new boolean atom.
+     *
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        if (is_bool($value) === false) {
+            throw new AtomException(get_class(), $value);
+        }
+
+        $this->value = (int) $value;
+    }
+
+    /**
+     * @see AtomInterface
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
