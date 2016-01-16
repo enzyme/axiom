e7b086d49bbb733e781a5659c8a6cce109dad50c
Tristan Strathearn <r3oath@gmail.com>
2016-01-16 16:31:47 +1000

    Add update method to factory interface.

--- src/Factories/FactoryInterface.php
+++ src/Factories/FactoryInterface.php
@@ -2,6 +2,7 @@
 
 namespace Enzyme\Axiom\Factories;
 
+use Enzyme\Axiom\Instances\InstanceInterface;
 use Enzyme\Axiom\Carriers\CarrierInterface;
 
 /**
@@ -17,4 +18,14 @@ interface FactoryInterface
      * @return Enzyme\Axiom\Instances\InstanceInterface
      */
     public function make(CarrierInterface $data);
+
+    /**
+     * Update the given instance with the data provided.
+     *
+     * @param InstanceInterface $instance Instance being updated.
+     * @param CarrierInterface  $data     The new data.
+     *
+     * @return Enzyme\Axiom\Instances\InstanceInterface
+     */
+    public function update(InstanceInterface $instance, CarrierInterface $data);
 }
