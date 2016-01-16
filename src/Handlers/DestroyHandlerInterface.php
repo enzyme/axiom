1f81c6043baa44c868409fa8e14801b95f9fa26e
Tristan Strathearn <r3oath@gmail.com>
2016-01-16 14:41:12 +1000

    Fix interface references on handlers due to container to report refactor.

--- src/Handlers/DestroyHandlerInterface.php
+++ src/Handlers/DestroyHandlerInterface.php
@@ -3,7 +3,7 @@
 namespace Enzyme\Axiom\Handlers;
 
 use Enzyme\Axiom\Instances\InstanceInterface;
-use Enzyme\Axiom\Containers\BadNewsInterface;
+use Enzyme\Axiom\Reports\ReportInterface;
 
 /**
  * Describes a class that will handle the positive or negative result
@@ -23,9 +23,9 @@ interface DestroyHandlerInterface
     /**
      * Called when the destroy operation was a failure.
      *
-     * @param BadNewsInterface $container The associated bad news.
+     * @param ReportInterface $report The associated failure message.
      *
      * @return mixed
      */
-    public function onDestroyFailure(BadNewsInterface $container);
+    public function onDestroyFailure(ReportInterface $report);
 }
