2f8dfb46938631d4756480b7d33df2900788af49
Tristan Strathearn <tstrathearn@bcm.com.au>
2016-01-19 16:02:41 +1000

    Add new exception types.

---
+++ src/Exceptions/FactoryException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Enzyme\Axiom\Exceptions;
+
+use Exception;
+
+/**
+ * Thrown when a factory cannot make or update a model.
+ */
+class FactoryException extends Exception
+{
+    //
+}
---
+++ src/Exceptions/RepositoryException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Enzyme\Axiom\Exceptions;
+
+use Exception;
+
+/**
+ * Thrown when a repository encouters an error.
+ */
+class RepositoryException extends Exception
+{
+    //
+}
